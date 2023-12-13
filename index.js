452f0d2e83d0cfb634696165f781d77318e84836
maopadev <dev.maopa@gmail.com>
2023-12-13 17:21:11 +0100

    Se aplica que muestre los datos del album

--- index.js
+++ index.js
@@ -6,33 +6,61 @@ const url = `https://www.discogs.com/es/`;
 
 const records = {};
 
-const getHTML = async () => {
+const getLatestsRecords = async () => {
   const res = await fetch(url);
   if (res.ok) {
     return await res.text();
   }
 };
 
-getHTML().then((response) => {
-    const $ = cheero.load(response);
-    $(".release_card").each((i, album) => {
-        const cover = $(album).find("img").attr("src");
-        const title = $(album).find("h4").text();
-        const artist = $(album).find("h5").text();
+const getInfoRecord = async (href) => {
+  const res = await fetch(href);
+  if (res.ok) {
+    return await res.text();
+  }
+};
 
-        let href = $(album).find('a').attr("href");
-        let index = href.match(/release\/(\d+)-/)[1];
+getLatestsRecords().then((resTitle) => {
+  const $ = cheero.load(resTitle);
+  $("#carousel-1 .release_card").each((i, album) => {
+    const cover = $(album).find("img").attr("src");
+    const title = $(album).find("h4").text();
+    const artist = $(album).find("h5").text();
 
-        records[index] = {
-            id: i,
-            cover: cover,
-            title: title,
-            artist: artist,
-            href: href
+    const href = $(album).find("a").attr("href");
+    let index = href.match(/release\/(\d+)-/)[1];
+
+    
+    const information = {};
+    getInfoRecord(href).then((resInfo) => {
+      const $ = cheero.load(resInfo);
+      $(".content_pzwez .table_1fWaB tbody").each((j, info) => {
+        information[i] = {
+          label: $(info).find("tr td").eq(0).text(),
+          format: $(info).find("tr td").eq(1).text(),
+          country: $(info).find("tr td").eq(2).text(),
+          released: $(info).find("tr td").eq(3).text(),
+          genre: $(info).find("tr td").eq(4).text(),
+          style: $(info).find("tr td").eq(5).text(),
         };
+        console.log(information);
+      });
     });
-    fs.writeFile("./db/newest.json", JSON.stringify(records), (err) => {
-        if (err) console.log("Error");
-        console.log("The file has been saved!");
-    });
+
+    console.log(information[i]);
+
+    records[i] = {
+      id: index,
+      cover,
+      title,
+      artist,
+      information: information[i],
+    };
+  });
+
+  /** Registramos los  */
+  fs.writeFile("./db/records.json", JSON.stringify(records), (err) => {
+    if (err) console.log("Error");
+    console.log("The file has been saved!");
+  });
 });
